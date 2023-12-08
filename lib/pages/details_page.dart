7b5beef1f54e8d6fcba1e120a914e30fbb5082c3
eller152 <eller152@umn.edu>
2023-12-08 11:45:44 -0600

    updated button placement

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -14,26 +14,65 @@ class DetailsPage extends StatefulWidget {
 
 class _DetailsPageState extends State<DetailsPage> {
   @override
-  void initState() {
-      // TODO: implement initState
-      super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("BetaBooks"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => Navigator.of(context).pushNamed(
-            compare,
-            arguments: BookArgs(book: widget.book)
-          ), 
-          child: const Text('Compare Editions'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              widget.book.title ?? 'N/A',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Price: ${widget.book.price ?? 'N/A'}',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Rating: ${widget.book.rating ?? 'N/A'}/5',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Edition: ${widget.book.edition ?? 'N/A'}',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Publication Date: ${widget.book.publishDate != null ? _formatDate(widget.book.publishDate!) : 'N/A'}',
+              style: TextStyle(fontSize: 18),
+            ),
+            Spacer(), // Added Spacer to push button to the bottom
+            Center(
+              child: ElevatedButton(
+                onPressed: () => Navigator.of(context).pushNamed(
+                  compare,
+                  arguments: BookArgs(book: widget.book),
+                ),
+                child: const Text('Compare Editions'),
+              ),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  String _formatDate(DateTime date) {
+    return '${_getMonthName(date.month)} ${date.day}, ${date.year}';
+  }
+
+  String _getMonthName(int month) {
+    const monthNames = [
+      'January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+    return monthNames[month - 1];
   }
 }
