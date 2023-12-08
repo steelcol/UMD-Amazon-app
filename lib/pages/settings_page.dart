4eead191ac7a1c5767822632e7736bb894b86f6d
eller152 <eller152@umn.edu>
2023-12-08 12:56:40 -0600

    started to update the settings page and added login route

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -2,76 +2,97 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:beta_books/routing/routes.dart';
 
-class SettingsPage extends StatefulWidget {
+class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
-
-  @override
-  State<SettingsPage> createState() => _SettingsPageState();
-}
-
-class _SettingsPageState extends State<SettingsPage> {
-  @override
-  void initState() {
-      // TODO: implement initState
-      super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("BetaBooks"),
-      ),
-      drawer: Drawer(
-        child: CustomScrollView(
-          physics: const NeverScrollableScrollPhysics(),
-          slivers: <Widget> [
-            SliverToBoxAdapter(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  DrawerHeader(
-                    child: Column(
-                      children: [
-                      /*
-                        Image(
-                          image: NetworkImage(FirebaseAuth.instance.currentUser!.photoURL!) ??,
-                        ),
-                        */
-                        FirebaseAuth.instance.currentUser!.displayName != null
-                        ? Text(FirebaseAuth.instance.currentUser!.displayName!) 
-                        : const Text('User'),
-                      ],
-                    ),
-                  ),
-                  ListTile(
-                    title: const Text('Account'),
-                    onTap: () => Navigator.of(context).pushReplacementNamed(profile),
-                  ),
-                  ListTile(
-                    title: const Text('Calendar'),
-                    onTap: () => Navigator.of(context).pushReplacementNamed(calendar),
-                  ),
-                  ListTile(
-                    title: const Text('Videos'),
-                    onTap: () => Navigator.of(context).pushReplacementNamed(videos),
-                  ),
-                ],
-              ),
-            ),
-            const SliverFillRemaining(
-              hasScrollBody: false,
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: ListTile(
-                  title: Text('Settings'),
-                ),
-              ),
-            ),
-          ]
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, home);
+          },
         ),
       ),
-      body: Center(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 16.0),
+                Text(
+                  FirebaseAuth.instance.currentUser!.displayName ?? 'User',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const Divider(height: 24.0, thickness: 2.0),
+                ListTile(
+                  title: const Text('Profile'),
+                  onTap: () => Navigator.of(context).pushNamed(profile),
+                ),
+                ListTile(
+                  title: const Text('Calendar'),
+                  onTap: () => Navigator.of(context).pushNamed(calendar),
+                ),
+                ListTile(
+                  title: const Text('Videos'),
+                  onTap: () => Navigator.of(context).pushNamed(videos),
+                ),
+                ListTile(
+                  title: const Text('Logout'),
+                  onTap: () async {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context).pushReplacementNamed(login);
+                  },
+                ),
+                ListTile(
+                  title: const Text('Light/Dark'),
+                  onTap: () {
+                    // Implement your theme toggle logic here
+                    // Example using provider: context.read<ThemeProvider>().toggleTheme();
+                  },
+                ),
+                ListTile(
+                  title: const Text('Delete Account'),
+                  onTap: () async {
+                    bool shouldDelete = await showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: const Text('Confirm Account Deletion'),
+                        content: const Text('Are you sure you want to delete your account?'),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.of(context).pop(false),
+                            child: const Text('Cancel'),
+                          ),
+                          TextButton(
+                            onPressed: () => Navigator.of(context).pop(true),
+                            child: const Text('Delete'),
+                          ),
+                        ],
+                      ),
+                    );
+
+                    if (shouldDelete == true) {
+                      try {
+                        await FirebaseAuth.instance.currentUser!.delete();
+                        Navigator.of(context).pushReplacementNamed(login);
+                      } catch (error) {
+                        print('Error deleting account: $error');
+                      }
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
